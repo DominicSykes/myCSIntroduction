f74db3fce139049192b002dee4cd5924fd436e03
Dom (Archer) Sykes <sykesdominic@gmail.com>
2020-10-09 09:58:28 -0500

    C# Introcution of myself!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mr. Sykes";
+            Console.WriteLine("Hello! I am " + name);
+            Console.WriteLine("I am currently a webdeveloper or a webdesigner.");
+            Console.WriteLine("I have been learning the Swift and C# programming languages.");
+            Console.WriteLine("I have primarily been learning about computers from Vector Technology Institute");
+            Console.WriteLine(" where I have recieved my Computer Systems Technology Diploma in 2019.");
+            Console.WriteLine("Now I am getting or have already obtained my Computer Systems Technician Diploma!");
+            Console.WriteLine("I am very interested in computers and programming as when I was growing up ");
+            Console.WriteLine("I saw the world through technology and technology be improved significantly");
+            Console.WriteLine("I am dyslexic so I saw Technology as my only ticket into the world and I took it");
+            Console.WriteLine(" and look where I am now!");
+
+        }
+    }
+}
