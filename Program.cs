76494953d33fe6ec6d7edf4a0bc8069297f307c9
Dominic <65317380+DominicSykes@users.noreply.github.com>
2021-01-17 13:00:45 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace myIntroduction
 {
@@ -8,15 +8,18 @@ namespace myIntroduction
         {
             string name = "Mr. Sykes";
             Console.WriteLine("Hello! I am " + name);
-            Console.WriteLine("I am currently a webdeveloper or a webdesigner.");
-            Console.WriteLine("I have been learning the Swift and C# programming languages.");
+            Console.WriteLine("I am currently a webdesigner and a Computers Technician.");
+            Console.WriteLine("I have been learning how to problem solve computers\n ");
+            Console.WriteLine(" as well as dissasemble and reassemble computers. ]n");
+            Console.WriteLine("I have learnt C# programming languages.");
             Console.WriteLine("I have primarily been learning about computers from Vector Technology Institute");
             Console.WriteLine(" where I have recieved my Computer Systems Technology Diploma in 2019.");
             Console.WriteLine("Now I am getting or have already obtained my Computer Systems Technician Diploma!");
             Console.WriteLine("I am very interested in computers and programming as when I was growing up ");
             Console.WriteLine("I saw the world through technology and technology be improved significantly");
-            Console.WriteLine("I am dyslexic so I saw Technology as my only ticket into the world and I took it");
-            Console.WriteLine(" and look where I am now!");
+            Console.WriteLine("I am dyslexic so I saw Technology as my only ticket into the world and I took it.");
+            Console.WriteLine(" I am very glad that I have taken that opotunity to learn more about technology\n");
+            Console.WriteLine(" as well as about what I can really do.");
 
         }
     }
