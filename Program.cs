ec3dc10a5f3f8d11e46e50f9385ff36aa0b8f577
Dominic <65317380+DominicSykes@users.noreply.github.com>
2021-01-17 13:05:50 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,17 +9,20 @@ namespace myIntroduction
             string name = "Mr. Sykes";
             Console.WriteLine("Hello! I am " + name);
             Console.WriteLine("I am currently a webdesigner and a Computers Technician.");
-            Console.WriteLine("I have been learning how to problem solve computers\n ");
-            Console.WriteLine(" as well as dissasemble and reassemble computers. ]n");
-            Console.WriteLine("I have learnt C# programming languages.");
-            Console.WriteLine("I have primarily been learning about computers from Vector Technology Institute");
-            Console.WriteLine(" where I have recieved my Computer Systems Technology Diploma in 2019.");
-            Console.WriteLine("Now I am getting or have already obtained my Computer Systems Technician Diploma!");
-            Console.WriteLine("I am very interested in computers and programming as when I was growing up ");
-            Console.WriteLine("I saw the world through technology and technology be improved significantly");
-            Console.WriteLine("I am dyslexic so I saw Technology as my only ticket into the world and I took it.");
-            Console.WriteLine(" I am very glad that I have taken that opotunity to learn more about technology\n");
-            Console.WriteLine(" as well as about what I can really do.");
+            Console.WriteLine("I have been learning how to problem solve computers\n\n ");
+            Console.WriteLine(" as well as dissasemble and reassemble computers. \n\n");
+            Console.WriteLine("I have learnt C# programming languages.\n\n");
+            Console.WriteLine("I have primarily been learning about computers from Vector Technology Institute\n\n");
+            Console.WriteLine(" where I have recieved my Computer Systems Technology Diploma in 2019.\n\n");
+            Console.WriteLine("Now I am getting or have already obtained my Computer Systems Technician Diploma!\n\n");
+            Console.WriteLine("I am very interested in computers and programming as when I was growing up \n\n");
+            Console.WriteLine("I saw the world through technology and technology be improved significantly\n\n");
+            Console.WriteLine("I am dyslexic so I saw Technology as my only ticket into the world and I took it.\n");
+            Console.WriteLine(" I am very glad that I have taken that opotunity to learn more about technology\n\n");
+            Console.WriteLine(" as well as about what I can really do.\n\n\n\n\n");
+            Console.WriteLine("Now that I have graduated from Vector Technology Institute \n\n");
+            Console.WriteLine("I am more confident in what I want to do as a career, which is a Computer Systems Technician.");
+            Console.WriteLine("I am backing this up with the gaining of more knowledge from Durham College.\n\n");
 
         }
     }
